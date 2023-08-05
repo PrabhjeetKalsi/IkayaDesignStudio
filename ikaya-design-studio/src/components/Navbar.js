4ff6d84bf41d059c456e5c4a683d8dee0e5d2c2e
Prabhjeet <prabhjeetkalsi00@gmail.com>
2023-08-05 18:11:56 +0530

    [feat]: add navbar and logo setup

---
+++ ikaya-design-studio/src/components/Navbar.js
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <nav class="navbar">
+      <a class="navbar-brand" href="/">
+        Ikaya Design Studio
+      </a>
+    </nav>
+  );
+}
+
+export default Navbar;
