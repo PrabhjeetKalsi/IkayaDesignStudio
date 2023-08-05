d6bb0c3bb1bc4823fecb95b4c25ea4effbeb9fc2
Prabhjeet <prabhjeetkalsi00@gmail.com>
2023-08-05 18:21:51 +0530

    [feat]: add logo to navbar

--- ikaya-design-studio/src/components/Navbar.js
+++ ikaya-design-studio/src/components/Navbar.js
@@ -1,7 +1,10 @@
+import logo from "../images/logo.png";
+
 function Navbar() {
   return (
     <nav class="navbar">
       <a class="navbar-brand" href="/">
+        <img src={logo} width="60" height="40" />
         Ikaya Design Studio
       </a>
     </nav>
