08061c5562ffa5ba3af29d51b13037c1ec6c5c26
Prabhjeet <prabhjeetkalsi00@gmail.com>
2023-08-08 18:54:21 +0530

    [feat]: add template components

---
+++ ikaya-design-studio/src/components/Contact.js
@@ -0,0 +1,24 @@
+function Contact() {
+  return (
+    <div className="p-5">
+      <h3>Contact</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Lacus sed viverra
+        tellus in hac habitasse platea. Bibendum at varius vel pharetra vel
+        turpis nunc eget lorem. Orci a scelerisque purus semper eget duis at
+        tellus at. Nam at lectus urna duis convallis convallis tellus.
+        Adipiscing elit ut aliquam purus sit. Fermentum posuere urna nec
+        tincidunt. Nulla at volutpat diam ut venenatis tellus in. Mattis
+        ullamcorper velit sed ullamcorper morbi tincidunt ornare. Id leo in
+        vitae turpis massa. Consectetur libero id faucibus nisl tincidunt eget.
+        Mauris ultrices eros in cursus turpis. Ut sem nulla pharetra diam. Porta
+        lorem mollis aliquam ut. Nulla at volutpat diam ut venenatis tellus in
+        metus. Pellentesque dignissim enim sit amet venenatis urna cursus eget
+        nunc.
+      </p>
+    </div>
+  );
+}
+
+export default Contact;
--- ikaya-design-studio/src/pages/Home.js
+++ ikaya-design-studio/src/pages/Home.js
@@ -1,9 +1,15 @@
 import Navbar from "../components/Navbar";
+import About from "../components/About";
+import Work from "../components/Work";
+import Contact from "../components/Contact";
 
 function Home() {
   return (
     <div>
       <Navbar />
+      <About />
+      <Work />
+      <Contact />
     </div>
   );
 }
